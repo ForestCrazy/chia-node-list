ef9c524be56c88058a932b5e3a440e3ee447849e
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-05-24 21:10:32 +0700

    add chia-node-append project link

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <MDBContainer>
         <p className="fs-1 text-center">Chia Node List</p>
         <p className="text-center">API: <a href='https://github.com/ForestCrazy/chia-node-list-api'>chia-nost-list-api</a></p>
+        <p className='text-center'>Chia Node Append: <a href='https://github.com/ForestCrazy/chia-node-append'>chia-node-append</a></p>
         <AddNode />
         <NodeTable />
       </MDBContainer>
